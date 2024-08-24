57d0147efd03a35941c10b96c5b2e0617396c24a
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-08-24 11:41:50 +0500

    working on the functionality of chatbot

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 var list = document.getElementById("list");
 var typing = document.getElementById("typing");
+var send = document.getElementById("send");
+send.addEventListener("click", sendMessage);
+console.log(send.value);
 
 function sendMessage(){
    console.log(event.keyCode);
    if(event.keyCode === 13){
       var elem = event.target;
-      list.innerHTML += "<li class='right-side right'>" + elem.value + "</li>";
+      list.innerHTML += "<li class='right-side'>" + elem.value + "</li>";
       typing.style.display = "block";
-      if(["hi", "hello", "oye" , "moye"].indexOf(elem.value.toLowerCase()) !== -1){
+      if(["hi", "hello", "oye" , "moye", "assalam oo alikum"].indexOf(elem.value.toLowerCase()) !== -1){
          setTimeout(function(){
-            list.innerHTML += "<li class='left-side left'> Hello </li>"
+            list.innerHTML += "<li class='left-side'> Hello </li>"
          }, 2000)
          setTimeout(function(){
-            list.innerHTML += "<li class='left-side left'> How are you? </li>"
+            list.innerHTML += "<li class='left-side'> How are you? </li>"
             typing.style.display = "none";
          }, 4000)
          
-      }else if(["theek", "Alhamdulillah",  "thk", "set" , "mast"].indexOf(elem.value.toLowerCase()) !== -1){
+      }else if(["theek", "Alhamdulillah",  "thk", "set" , "mast", "fine" , "good", "great", "awesome", "fit"].indexOf(elem.value.toLowerCase()) !== -1){
          setTimeout(function(){
             list.innerHTML += "<li class='left-side'> How can I help you? </li>";
             typing.style.display = "none";
